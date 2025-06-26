f1defa9308210b958d87540c66a19ab9844f5b39
Thiago Antonio Ramalho <thiagofacul@gmail.com>
2025-06-26 07:03:43 -0300

    Components, Layout e Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
     <div className={
       `
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-blue-500 to-purple-500 text-white
+      bg-gradient-to-r from-blue-500 to-purple-500
+       text-white
       `
     }>
-      <h1 className="text-4xl font-bold mb-4">Welcome to My Next.js App</h1>
-      <br />
-      <h2 className="text-lg text-gray-700">This is a simple page using Tailwind CSS.</h2>
+      <Layout titulo="Welcome to My Next.js App">
+        <span>This is a simple page using Tailwind CSS.</span>
+      </Layout>
     </div>
   );
 }
