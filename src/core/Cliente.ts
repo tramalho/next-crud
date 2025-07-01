da5fcf2f891c797b9852e8174af008ed5dccc4a3
Thiago Antonio Ramalho <thiagofacul@gmail.com>
2025-07-01 07:15:40 -0300

    Classe Cliente

---
+++ src/core/Cliente.ts
@@ -0,0 +1,25 @@
+export default class Cliente {
+
+    #id: string
+    #nome: string
+    #idade: number
+
+    constructor(id: string, nome: string, idade: number = 0) {
+        this.#id = id
+        this.#nome = nome
+        this.#idade = idade
+    }
+
+    static vazio() {
+        return new Cliente('', '', '')
+    }
+    get id() {
+        return this.#id
+    }
+    get nome() {
+        return this.#nome
+    }
+    get idade() {
+        return this.#idade
+    }
+}
