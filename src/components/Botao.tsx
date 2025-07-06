66a6aed858f7ccbb76b8dd22c287d0908a06e301
Thiago Antonio Ramalho <thiagofacul@gmail.com>
2025-07-06 08:12:42 -0300

    Compontente Botão

---
+++ src/components/Botao.tsx
@@ -0,0 +1,31 @@
+interface BotaoProps {
+  cor: "blue" | "gray" | "green";
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Botao(props: BotaoProps) {
+  return (
+    <button
+      className={`
+            bg-gradient-to-r ${cor()}
+            hover:opacity-80
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}
+    >
+      {props.children}
+    </button>
+  );
+
+  function cor() {
+    switch (props.cor) {
+      case "blue":
+        return "from-blue-400 to-blue-700";
+      case "green":
+        return "from-green-400 to-green-700";
+      default:
+        return "from-gray-400 to-gray-700";
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
+import Botao from "@/components/Botao";
 
 export default function Home() {
   const clientes = [
@@ -28,6 +29,9 @@ export default function Home() {
       `}
     >
       <Layout titulo="Cadastro Simples">
+        <div className={`flex justify-end`}>
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela 
         clientes={clientes} 
         clienteSelecionado={clienteSelecionado} 
